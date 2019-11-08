e1d29979e8cb42c45ea557e47383511d66a730a7
dongchul <sah3122@naver.com>
2019-11-08 13:12:33 +0900

    ADD Project Init

---
+++ src/main/java/me/study/spring_boot_get_start/SpringBootGetStartApplication.java
@@ -0,0 +1,13 @@
+package me.study.spring_boot_get_start;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootGetStartApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootGetStartApplication.class, args);
+    }
+
+}
---
+++ src/test/java/me/study/spring_boot_get_start/SpringBootGetStartApplicationTests.java
@@ -0,0 +1,13 @@
+package me.study.spring_boot_get_start;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootGetStartApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
